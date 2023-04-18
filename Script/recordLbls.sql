6d547fb400c60e85890ec97474a7419377b959bc
912-Cristea-Aida <aida.cristea@stud.ubbcluj.ro>
2023-04-18 14:34:42 +0300

    pagination

--- Script/recordLbls.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO record_lables (address, name_rl,nr_collaborations, price, review ) VALUES ('99566 Johnson Junction Apt. 727
-North Tonyastad, MP 38818', 'Richard Thompson', 72, 1789, 'Wonderful');
-INSERT INTO record_lables (address, name_rl,nr_collaborations, price, review ) VALUES ('2625 Samuel Locks
-Seanside, KS 17168', 'Christine Blair', 148, 4688, 'Amazing');
